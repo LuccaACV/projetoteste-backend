ce9a67003bf88591d137d0aa38549d73bf989f3f
Lucca Amaral <lucca.valente@gmail.com>
2024-11-24 19:03:15 -0300

    Add: Primeira versão do Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,33 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
 
 export const app = fastify()
 
-app.register(appRoutes)
+// Registrar o Swagger
+app.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Acomodações API',
+        description: 'API para gerenciar acomodações.',
+        version: '1.0.0',
+      },
+    },
+  });
+  
+  // Registrar o Swagger UI
+  app.register(fastifySwaggerUi, {
+    routePrefix: '/documentation', // URL onde a documentação estará disponível
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false,
+    },
+    staticCSP: true,
+    transformSpecification: (swaggerObject, request, reply) => swaggerObject,
+    transformSpecificationClone: true,
+  });
+
+
+  app.register(appRoutes)
