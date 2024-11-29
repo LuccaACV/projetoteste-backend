41b407945f065e004e83a56afd5c0ab6487be41c
Victor Bastos <vbasx@outlook.com>
2024-11-29 11:39:40 -0300

    refactor: teste de endereço por IP

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,35 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { appRoutes } from "./http/routes";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 
-
 export const app = fastify()
 
-// Registrar o Swagger
+app.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+})
+
 app.register(fastifySwagger, {
-    openapi: {
-      info: {
-        title: 'Acomodações API',
-        description: 'API para gerenciar acomodações.',
-        version: '1.0.0',
-      },
+  openapi: {
+    info: {
+      title: 'Acomodações API',
+      description: 'API para gerenciar acomodações.',
+      version: '1.0.0',
     },
-  });
-  
-  // Registrar o Swagger UI
-  app.register(fastifySwaggerUi, {
-    routePrefix: '/documentation', // URL onde a documentação estará disponível
-    uiConfig: {
-      docExpansion: 'full',
-      deepLinking: false,
-    },
-    staticCSP: true,
-    transformSpecification: (swaggerObject, request, reply) => swaggerObject,
-    transformSpecificationClone: true,
-  });
+  },
+});
 
+app.register(fastifySwaggerUi, {
+  routePrefix: '/documentation',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false,
+  },
+  staticCSP: true,
+  transformSpecification: (swaggerObject, request, reply) => swaggerObject,
+  transformSpecificationClone: true,
+});
 
-  app.register(appRoutes)
+app.register(appRoutes)
